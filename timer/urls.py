da432465cf005956175fec296ec48b6e77320598
Tryhardzzz <110442154+Tryhardzzz@users.noreply.github.com>
2023-03-12 22:57:10 -0400

    Add files via upload

--- timer/urls.py
+++ timer/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	path('', views.times_list, name='times_list'),
 	path('time/<int:pk>/', views.time_detail, name='time_detail'),
+	path('time/<int:pk>/edit/', views.time_edit, name='time_edit')
 ]
--- timer/views.py
+++ timer/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from .models import Timer
 from .forms import TimeForm
+from django.utils import timezone
 
 def get(request):
         form = TimeForm()
@@ -20,3 +21,17 @@ def time_detail(request, pk):
 	form = TimeForm(request.POST)
 	times = get_object_or_404(Timer, pk=pk)
 	return render(request, 'timer/time_detail.html', {'form': form, 'times': times})
+	
+def time_edit(request, pk):
+	print("Primary key: ", pk)
+	times = get_object_or_404(Timer, pk=pk)
+	if request.method == "POST":
+		form = TimeForm(request.POST, instance=times)
+		if form.is_valid():
+		    times = form.save(commit=False)
+		    times.date = timezone.now()
+		    times.save()
+		    return redirect('times_list')
+	else:
+		form = TimeForm(instance=times)
+	return render(request, 'timer/time_edit.html', {'form': form, 'times': times})
